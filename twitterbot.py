5acf46db3500dd3cdf55c3939ba3604c0361eb2d
John Q. Herman <johnqherman@gmail.com>
2023-04-04 22:17:51 -0400

    Refactored and optimized camera fetching and posting logic
    This commit significantly refactors the codebase, breaking it down into modular functions for better readability and maintainability. The previous method of loading cameras has been replaced with a new function that fetches camera links live from the Insecam sitemap. Additionally, the code now uses the lxml library for improved HTML parsing performance, and the OpenCV library to check for solid grey images, allowing the script to skip posting such images to Twitter. Error handling has also been improved, with better retry logic for downloading images and more informative error messages.

--- twitterbot.py
+++ twitterbot.py
@@ -3,149 +3,233 @@ import os
 import random
 import time
 
+import cv2
 import requests
 import tweepy
-from bs4 import BeautifulSoup
+from lxml import etree, html
+from requests.exceptions import ReadTimeout, RequestException, Timeout
 
-# twitter api credentials
-with open('data/credentials.csv', 'r') as f:
-    credentials = list(csv.reader(f))
-    auth = tweepy.OAuthHandler(credentials[0][0], credentials[0][1])
-    auth.set_access_token(credentials[0][2], credentials[0][3])
-    api = tweepy.API(auth)
 
-while True:
-    # get cameras from db file
-    with open('data/cams.csv', 'r') as f:
-        cams = list(csv.reader(f))
+def load_credentials():
+    with open('credentials.csv', 'r') as f:
+        return list(csv.reader(f))
 
-    url = random.choice(cams)[0] # pick a random camera
-    camera_id = ''.join(c for c in url if c.isdigit()) # assign unique camera id
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4'} # spoof user agent
-    r = requests.get(url, headers=headers) # get camera page
-    html = r.text # get html as text
-    soup = BeautifulSoup(html, 'html.parser') # parse html
-    camera_url = soup.find('img') # find camera feed
 
-    # make sure url isn't NoneType object
-    if camera_url is not None:
-            camera_url = camera_url.get('src').replace("?COUNTER", "")
+def load_cameras():
+    r = requests.get("http://www.insecam.org/static/sitemap.xml")
+
+    if r.status_code == 200:
+        loc_elements = [link for link in etree.fromstring(
+            r.content).iter('{*}loc')]
+        camera_links = [link.text for link in loc_elements]
+        print(f"fetched {len(camera_links)} camera links.")
     else:
-        print("camera URL is None. skipping...")
-        continue
+        print("failed to fetch camera links.")
+        return None
 
-    # check camera format
-    if camera_url != "/static/no.jpg" \
-        and ".mjpg" not in camera_url \
-        and "?action=stream" not in camera_url \
-        and "wvhttp" not in camera_url \
-        and ".cgi" not in camera_url \
+    return camera_links
+
+
+def get_random_camera_url(camera_links):
+    random_camera_url = random.choice(camera_links)
+    return random_camera_url
+
+
+def get_camera_id(random_camera_url):
+    return ''.join(c for c in random_camera_url if c.isdigit())
+
+
+def get_camera_page(random_camera_url, headers):
+    try:
+        r = requests.get(random_camera_url, headers=headers)
+        return r.content
+    except (requests.exceptions.RequestException, OSError) as e:
+        print("error capturing image: " + str(e))
+        return None
+
+
+def find_camera_url(tree):
+    camera_url = tree.xpath('//img/@src')
+    return camera_url[0].replace("?COUNTER", "") if camera_url else None
+
+
+def is_valid_camera(camera_url):
+    return camera_url != "/static/no.jpg" \
         and "?stream" not in camera_url \
-        and ".jpg" in camera_url:
-        print('camera accepted: ' + camera_id)
-    else:
-        print('camera rejected: ' + camera_id)
-        continue
+        and ".jpg" in camera_url
 
-    # grab detailed camera info
-    details = soup.find_all('div', class_='camera-details')
-    details_array = []
-    for detail in details:
-        details_array.append(detail.text)
 
-    # remove extra newlines, tabs, and whitespace
-    details = str([detail.replace('\n', '').replace('\t', '').strip() for detail in details_array])
+def get_camera_details(tree):
+    details = tree.xpath('//div[@class="camera-details"]')
+    details_array = [detail.text_content() for detail in details]
+    details = ''.join(detail.replace('\n', '').replace(
+        '\t', '').strip() for detail in details_array)
+    return details
 
-    # store camera details
+
+def parse_camera_details(details):
     camera_info = {
         "city": details[details.find("City: ") + len("City: "):details.find("Latitude:")],
         "region": details[details.find("Region:") + len("Region:"):details.find("City:")],
         "country": details[details.find("Country:") + len("Country:"):details.find("Country code:")],
         "country_code": details[details.find("Country code:") + len("Country code:"):details.find("Region:")]
     }
+    return camera_info
 
-    # shorten country names
-    country = camera_info['country']\
-        .replace(", Province Of", "")\
-        .replace(", Republic Of", "")\
-        .replace(", Islamic Republic", "")\
-        .replace("n Federation", "")\
-        .replace("ian, State Of", "e")
 
+def shorten_country_name(country):
+    return (country
+            .replace(", Province Of", "")
+            .replace(", Republic Of", "")
+            .replace(", Islamic Republic", "")
+            .replace("n Federation", "")
+            .replace("ian, State Of", "e"))
+
+
+def assemble_flag_emoji(country_code):
     symbols = {
-            'A': '🇦',
-            'B': '🇧',
-            'C': '🇨',
-            'D': '🇩',
-            'E': '🇪',
-            'F': '🇫',
-            'G': '🇬',
-            'H': '🇭',
-            'I': '🇮',
-            'J': '🇯',
-            'K': '🇰',
-            'L': '🇱',
-            'M': '🇲',
-            'N': '🇳',
-            'O': '🇴',
-            'P': '🇵',
-            'Q': '🇶',
-            'R': '🇷',
-            'S': '🇸',
-            'T': '🇹',
-            'U': '🇺',
-            'V': '🇻',
-            'W': '🇼',
-            'X': '🇽',
-            'Y': '🇾',
-            'Z': '🇿'
-        }
+        'A': '🇦',
+        'B': '🇧',
+        'C': '🇨',
+        'D': '🇩',
+        'E': '🇪',
+        'F': '🇫',
+        'G': '🇬',
+        'H': '🇭',
+        'I': '🇮',
+        'J': '🇯',
+        'K': '🇰',
+        'L': '🇱',
+        'M': '🇲',
+        'N': '🇳',
+        'O': '🇴',
+        'P': '🇵',
+        'Q': '🇶',
+        'R': '🇷',
+        'S': '🇸',
+        'T': '🇹',
+        'U': '🇺',
+        'V': '🇻',
+        'W': '🇼',
+        'X': '🇽',
+        'Y': '🇾',
+        'Z': '🇿'
+    }
+    return "".join(symbols.get(char, char) for char in country_code)
 
-    # assemble flag emoji from regional indicator symbols
-    flag = camera_info["country_code"]
-    for char, replacement in symbols.items():
-        flag = flag.replace(char, replacement)
 
-    # save image to disk
-    image_path = str(camera_id) + "_" + str(int(time.time())) + ".jpg"
-    with open(image_path, 'wb') as f:
+def save_image(image_path, camera_url, headers, retries=3):
+    for attempt in range(retries):
         try:
-            print("attempting to capture image...")
-            r = requests.get(camera_url, headers=headers, timeout=5)
-            f.write(r.content)
-        except(ConnectionError, TimeoutError, OSError) as e:
-            print("error capturing image: " + str(e))
-            os.remove(image_path)
-            continue
-    if os.stat(image_path).st_size == 0:
-        print("image is empty")
+            r = requests.get(camera_url, headers=headers, timeout=10)
+            if r.status_code == 200:
+                with open(image_path, 'wb') as f:
+                    f.write(r.content)
+                return True
+        except (RequestException, ReadTimeout, Timeout) as e:
+            print(f"error downloading image: {e}")
+            if attempt < retries - 1:
+                print(f"retrying... (attempt {attempt + 1})")
+            else:
+                print("download failed after multiple attempts.")
+                return False
+    return False
+
+
+def image_is_solid_grey(image_path):
+    img = cv2.imread(image_path)
+    if (img == [16, 16, 16]).all():
         os.remove(image_path)
-        continue
+        return True
+    return False
 
-    # assemble location string
+
+def create_tweet_text(camera_info, flag):
+    city = camera_info['city'] if camera_info['city'] != "-" else "Unknown"
+    region = camera_info['region'] if camera_info['region'] != "-" else "Unknown"
+    country = camera_info['country'] if camera_info['country'] != "-" else "Unknown"
+
     if country == "United States":
-        location = camera_info['city'] + ", " + camera_info['region']
+        location = city + ", " + region
     elif country == "Canada":
-        location = camera_info['city'] + ", " + camera_info['region'] + ", " + country
+        location = city + ", " + region + ", " + country
     else:
-        location = camera_info['city'] + ", " + country
+        location = city + ", " + country
 
-    # set tweet text
-    if location == "-, -" or "," not in location:
-        status = "Unknown Location"
+    if city == "Unknown" and region == "Unknown" and country == "United States":
+        return "Unknown, United States " + flag
+    elif location == "Unknown, Unknown":
+        return "Unknown Location"
     else:
-        status = location + " " + flag
+        return location + " " + flag
 
-    # post to twitter
+
+def post_to_twitter(api, status, image_path):
     try:
         print("posting to twitter...")
         api.update_status_with_media(status=status, filename=image_path)
-    except tweepy.TweepyException as e:
+        latest_tweet_id = api.user_timeline(count=1)[0].id
+        tweet_url = f"https://twitter.com/Unsecured_CCTV/status/{latest_tweet_id}"
+        print(f"post successful: {tweet_url}")
+        return True
+    except tweepy.TweepError as e:
         print("post failed: " + str(e))
-        continue
+        return False
 
-    # delete image, wait an hour, and repeat
-    print("post successful: https://twitter.com/Unsecured_CCTV/status/" + str(api.user_timeline(count=1)[0].id) + "\n" + "waiting an hour...")
-    os.remove(image_path)
-    time.sleep(3600)
-    continue
+
+def main():
+
+    # twitter credentials
+    credentials = load_credentials()
+    auth = tweepy.OAuthHandler(credentials[0][0], credentials[0][1])
+    auth.set_access_token(credentials[0][2], credentials[0][3])
+    api = tweepy.API(auth)
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4'}
+
+    cameras_list = load_cameras()
+
+    while True:
+        random_camera_url = get_random_camera_url(cameras_list)
+        random_camera_id = get_camera_id(random_camera_url)
+        camera_page_content = get_camera_page(random_camera_url, headers)
+
+        if not camera_page_content:
+            continue
+
+        camera_page_tree = html.fromstring(camera_page_content)
+        camera_stream_url = find_camera_url(camera_page_tree)
+
+        if not camera_stream_url or not is_valid_camera(camera_stream_url):
+            print(f'camera rejected: {random_camera_id}')
+            continue
+
+        print(f'camera accepted: {random_camera_id}')
+
+        details = get_camera_details(camera_page_tree)
+        camera_info = parse_camera_details(details)
+        camera_info['country'] = shorten_country_name(camera_info['country'])
+        flag = assemble_flag_emoji(camera_info['country_code'])
+
+        image_path = f"{random_camera_id}_{int(time.time())}.jpg"
+        saved_successfully = save_image(image_path, camera_stream_url, headers)
+
+        if not saved_successfully:
+            print("failed to save the image. skipping...")
+            continue
+
+        if image_is_solid_grey(image_path):
+            print("image is solid grey. skipping...")
+            continue
+
+        status = create_tweet_text(camera_info, flag)
+        posted_successfully = post_to_twitter(api, status, image_path)
+
+        if posted_successfully:
+            print("waiting for an hour...")
+            time.sleep(60 * 60)
+
+
+if __name__ == "__main__":
+    main()

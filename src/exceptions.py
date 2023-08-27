e675d6b6baed2836f7dc38e5b696f67e1a7dee9f
John Q. Herman <johnqherman@gmail.com>
2023-08-27 19:38:09 -0400

    Remove SaveImageException class
    With the integration of Selenium, the need for this custom exception has been eliminated. Selenium handles the respective functionality internally, making this custom exception redundant.

--- src/exceptions.py
+++ src/exceptions.py
@@ -6,13 +6,3 @@ class FetchCamerasException(Exception):
 
     def __str__(self) -> str:
         return repr(self.message)
-
-
-class SaveImageException(Exception):
-    """Custom exception class for handling errors when saving camera images."""
-
-    def __init__(self, message: str):
-        self.message = message
-
-    def __str__(self) -> str:
-        return repr(self.message)
